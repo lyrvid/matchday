79250098d9408d342428567ffd827b9417b4903d
Lei Ferretti <elisa.c.ferretti@gmail.com>
2023-05-12 16:09:09 +0100

    very cheeky commit after the fact to add forgotten indexes... feel free to ignore this one.

--- matchday/models.py
+++ matchday/models.py
@@ -5,6 +5,11 @@ class Author(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200, blank=True)
 
+    class Meta:
+        indexes = [
+            models.Index(fields=['last_name', 'first_name'])
+        ]
+
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
@@ -12,6 +17,11 @@ class Author(models.Model):
 class Quote(models.Model):
     quote = models.TextField()
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['author'])
+        ]
 
     def __str__(self):
         return f"\"{self.quote}\" - {self.author}"
